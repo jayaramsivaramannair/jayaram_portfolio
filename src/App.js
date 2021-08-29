d1b71972724415a65ccb1c9737ff09b7c0560a67
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-08-29 19:33:04 -0400

    Added Navbar, About, Projects, Skills and Contact Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 export default function App() {
-  return "App";
+  return (
+    <div>
+      <Navbar/>
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </div>
+  )
 }
