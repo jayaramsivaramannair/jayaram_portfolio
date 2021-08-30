64508e759340651ea203e2b030de86b22bd49b82
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-08-29 21:00:34 -0400

    Added a form to the contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,40 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
-      Contact Section
-    </div>
+    <section id="contact">
+      <div>
+          <form netlify name="contact">
+            <h2>
+              Hire Me
+            </h2>
+            <p></p>
+            <div>
+              <label htmlFor="name">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+              />
+            </div>
+            <div>
+              <label htmlFor="message">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+              />
+            </div>
+            <button
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+    </section>
   )
 }
 
