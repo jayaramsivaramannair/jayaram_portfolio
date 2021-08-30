cc8ee132005f66df30529586b249ae625dc0aef6
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-08-29 20:07:46 -0400

    Added content to projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import {projects} from "../data";
 
 function Projects() {
   return (
-    <div>
-      Projects Section
-    </div>
+    <section id="projects">
+      <div>
+        <div>
+          <h1>
+            Projects
+          </h1>
+          <p>
+            Technologies which I have used in making of these projects
+          </p>
+        </div>
+        <div>
+          {projects.map((project) => {
+            return <a href={project.link} key={project.image}>
+              <div className="project-gallery">
+                <img src={project.image} alt={project.description}/>
+                <div>
+                  <h2>
+                    {project.subtitle}
+                  </h2>
+                  <h1>
+                    {project.title}
+                  </h1>
+                  <p>{project.description}</p>
+                </div>
+              </div>
+            </a>
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
